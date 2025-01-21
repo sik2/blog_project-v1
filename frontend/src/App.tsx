60897b7825b2ed93e2b78d9d08d18bf018b024db
sik2 <bickwang123@gmail.com>
2025-01-21 11:20:25 +0900

    work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,11 @@
 function App() {
+  console.log(import.meta.env.VITE_CORE_FRONT_BASE_URL);
+  console.log(import.meta.env.VITE_CORE_API_BASE_URL);
+
+  fetch(import.meta.env.VITE_CORE_FRONT_BASE_URL + "/api/v1/test")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <>
       <h1>Hello World!!!</h1>
