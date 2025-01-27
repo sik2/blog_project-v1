49ea12024ba42c36b3f39648db6b123fcf144ae0
sik2 <bickwang123@gmail.com>
2025-01-27 16:19:56 +0900

    test oauth2

--- backend/src/main/java/com/ll/blog/domain/home/main/controller/MainController.java
+++ backend/src/main/java/com/ll/blog/domain/home/main/controller/MainController.java
@@ -1,9 +1,14 @@
 package com.ll.blog.domain.home.main.controller;
 
-import lombok.Getter;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class MainController {
@@ -21,5 +26,23 @@ public class MainController {
         System.out.println("frontUrl = " + frontUrl);
 
         return "Welcome to Main Page";
+    }
+
+    @GetMapping("/session")
+    @ResponseBody
+    public String session(HttpSession session) {
+        String sessionDump = Stream.iterate(
+                        session.getAttributeNames().asIterator(),
+                        Iterator::hasNext,
+                        it -> it
+                ).flatMap(it -> Stream.of(it.next()))
+                .map(attributeName -> {
+                    Object attributeValue = session.getAttribute(attributeName);
+                    return attributeName + " = " + attributeValue;
+                })
+                .collect(Collectors.joining("\n", "Session Attributes:\n", ""));
+
+        // 완성된 세션 정보 반환
+        return sessionDump;
     }
 }
