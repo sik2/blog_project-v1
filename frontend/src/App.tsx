7f3bf247fb3b3e2c4640c086def3bd1e84605eb0
sik2 <bickwang123@gmail.com>
2025-01-27 17:07:50 +0900

    oauth2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 function App() {
-  console.log(import.meta.env.VITE_CORE_FRONT_BASE_URL);
-  console.log(import.meta.env.VITE_CORE_API_BASE_URL);
+  // console.log(import.meta.env.VITE_CORE_FRONT_BASE_URL);
+  // console.log(import.meta.env.VITE_CORE_API_BASE_URL);
 
-  fetch(import.meta.env.VITE_CORE_FRONT_BASE_URL + "/api/v1/test")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const socialLoginForKakaoUrl = `http://localhost:8090/oauth2/authorization/kakao`; // 카카오 로그인 요청 URL
+  const redirectUrlAfterSocialLogin = "http://localhost:3000"; // 카카오 로그인 후 리다이렉트 URL
 
   return (
     <>
-      <h1>Hello World!!!</h1>
+      <a
+        href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
+      >
+        <span>카카오 로그인</span>
+      </a>
     </>
   );
 }
